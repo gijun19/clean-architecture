5a64a16e970b15e9ab6366e7b61016ab69e30b4f
Daniel Park <daniel.park@endevors.io>
2020-11-09 07:24:06 -0800

    feat: update base mapper

--- packages/ca-core/src/infra/BaseMapper.ts
+++ packages/ca-core/src/infra/BaseMapper.ts
@@ -1,4 +1,4 @@
 export abstract class BaseMapper<DomainEntity, DatabaseModel = any> {
-  protected abstract toDb(entity: DomainEntity): Promise<DatabaseModel>;
-  protected abstract toDomain(model: DatabaseModel): Promise<DomainEntity>;
+  protected abstract toDb(entity: DomainEntity): DatabaseModel;
+  protected abstract toDomain(model: DatabaseModel): DomainEntity;
 }
